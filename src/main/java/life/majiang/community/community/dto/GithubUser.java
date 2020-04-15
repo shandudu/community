902fb4ca6e11fc90367fdd4302d80caad386c1e4
tom_glb <goodMorning_glb@atguigu.com>
2020-04-15 21:33:19 +0800

    extract github config from code to  properties

---
+++ src/main/java/life/majiang/community/community/dto/GithubUser.java
@@ -0,0 +1,35 @@
+package life.majiang.community.community.dto;
+
+/**
+ * @author
+ * @data 2020/4/15 0015 - 18:32
+ **/
+public class GithubUser {
+    private String name;
+    private Long id;
+    private String bio;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
